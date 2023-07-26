ebb7576e29ccb688f83cc2e0160e788dd60efdc5
Mariam Qassem <55749889+mariam404@users.noreply.github.com>
2023-07-26 18:34:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const request = require ("request")
+const forecast = require ("./forecast")
+const geocode = require ('./geocode')
+
+
+const country = process.argv[2]
+
+geocode ( country , (error , data) => {
+    console.log("ERROR : " , error)
+    console.log("DATA  : " , data)
+
+    if (data) {
+        forecast( data.latitude , data.longtitude , (error , data) => {
+            console.log("ERROR : " , error)
+            console.log("DATA  : " , data)
+       } )
+    } else {
+        console.log("Data Entered have An Error")
+    }
+   
+} ) 
